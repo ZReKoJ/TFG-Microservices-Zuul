59d3d3b0b4141960b1cc107fd616a022f1d70722
ZReKoJ <zhong.9745@gmail.com>
2019-02-12 16:25:15 +0100

    test for https routes

--- src/main/java/es/ucm/fdi/tfg/app/App.java
+++ src/main/java/es/ucm/fdi/tfg/app/App.java
@@ -1,19 +1,15 @@
 package es.ucm.fdi.tfg.app;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@Controller
+@SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class App {
-  public static void main(String[] args) {
-    new SpringApplicationBuilder(App.class).web(true).run(args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
