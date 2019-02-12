318f81a51e5b10b14a630c9a23580c27cfa4ac49
ZReKoJ <zhong.9745@gmail.com>
2019-02-12 16:16:12 +0100

    minor changes

--- src/main/java/es/ucm/fdi/tfg/app/App.java
+++ src/main/java/es/ucm/fdi/tfg/app/App.java
@@ -1,15 +1,19 @@
 package es.ucm.fdi.tfg.app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
 @EnableZuulProxy
 public class App {
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(App.class).web(true).run(args);
+  }
 }
